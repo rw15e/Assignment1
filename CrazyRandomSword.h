a4bb57b4bc67b6e1fe374dbbc8c660cc2aca02c0
rw15e <rw15e@my.fsu.edu>
2017-10-10 10:33:34 -0400

    Create CrazyRandomSword.h

---
+++ CrazyRandomSword.h
@@ -0,0 +1,27 @@
+// Ryan Winter
+// 10/10/17
+// CEN4020 HW2
+
+/*
+#include <string>
+#include "Weapon.h"
+
+#ifndef COMMONSPEAR_H
+#define COMMONSPEAR_H
+
+/**
+ * Defines the behavior of a common spear (hitpoint = 40, ignores 20% of 
+ * armor points)
+ */
+class CommonSpear : public Weapon {
+public:
+
+    CommonSpear() : Weapon("Common spear", 40.0) {
+    }
+    virtual ~CommonSpear() {}; 
+    virtual double hit(double armor);
+
+};
+
+#endif /* COMMONSPEAR_H */
+*/
