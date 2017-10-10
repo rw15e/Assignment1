7db6a2a806384f461022f675f318a1f75ce88fa0
rw15e <rw15e@my.fsu.edu>
2017-10-10 10:34:22 -0400

    Create CrazyRandomSword.cpp

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,18 @@
+// Ryan Winter
+// 10/10/17
+// CEN4020 HW2
+
+
+/*
+#include "CommonSword.h"
+
+
+double CommonSword::hit(double armor){
+    double damage = hitPoints - armor;
+    if(damage < 0){
+        return 0;
+    }
+    return damage;
+}
+
+*/
