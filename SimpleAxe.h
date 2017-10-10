a302fb27145347ec90cb1691fcbaf207d0574b29
rw15e <rw15e@my.fsu.edu>
2017-10-10 10:36:04 -0400

    Create SimpleAxe.h

---
+++ SimpleAxe.h
@@ -0,0 +1,27 @@
+// Ryan Winter
+// 10/10/17
+// CEN4020 HW2
+
+
+/*
+#include <string>
+#include "Weapon.h"
+
+#ifndef COMMONSWORD_H
+#define COMMONSWORD_H
+
+/**
+ * Defines the behavior of a common sword (hitpoint = 50, it does not ignore armor points)
+ */
+class CommonSword : public Weapon {
+public:
+
+    CommonSword() : Weapon("Common sword", 50.0) {
+    }
+    virtual ~CommonSword() {}; 
+    virtual double hit(double armor);
+
+};
+
+#endif /* COMMONSWORD_H */
+*/
