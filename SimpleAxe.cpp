6a0e175974d2faa11df39e4a03f2764e5f607143
rw15e <rw15e@my.fsu.edu>
2017-10-11 10:18:15 -0400

    Update SimpleAxe.cpp
    implement SimpleAxe

--- SimpleAxe.cpp
+++ SimpleAxe.cpp
@@ -2,19 +2,13 @@
 // 10/10/17
 // CEN4020 HW2
 
+#include "SimpleAxe.h"
 
+//SimpleAxe (5 points): hitpoints = 60. If armor is greater than 0 and less than 20,
+//the axe will ignore all the armor points.
 
-/*
-#include "CommonSword.h"
-
-
-double CommonSword::hit(double armor){
-    double damage = hitPoints - armor;
-    if(damage < 0){
-        return 0;
-    }
-    return damage;
+double SimpleAxe::hit(double armor){
+	if (armor > 0 && armor < 20)
+		armor = 0;
+	return hitPoints - armor;
 }
-
-
-*/
