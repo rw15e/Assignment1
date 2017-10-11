a6c71aae0ce0a94799900537c1b441a943cfb4a5
rw15e <rw15e@my.fsu.edu>
2017-10-11 10:29:38 -0400

    Create CommonBaseBallBat.h
    Implement custom weapon header file

---
+++ CommonBaseBallBat.h
@@ -0,0 +1,24 @@
+// Ryan Winter
+// 10/10/17
+// CEN4020 HW2
+
+#include "Weapon.h"
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+#include <math.h>
+
+#ifndef COMMONBASEBALLBAT_H
+#define COMMONBASEBALLBAT_H
+
+//CommonBaseBallBat hitpoints = 40 and has a 25% chance to ignore all the armor.
+
+class CrazyRandomSword : public Weapon{
+    public:
+        CrazyRandomSword() : Weapon("Common Baseball Bat", 0){};
+        virtual ~CrazyRandomSword({};
+	      virtual double hit(double armor);
+    
+}
+
+#endif
