10d592eb593a63a6f0ae6c172dcadd8beae7fdb8
rw15e <rw15e@my.fsu.edu>
2017-10-11 10:10:00 -0400

    Update CrazyRandomSword.cpp
    Completed CrazyRandomSword.cpp

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -5,25 +5,13 @@
 #include "CrazyRandomSword.h"
 
 
-
-
 //CrazyRandomSword (10 points): hitpoints = random integer number between 10
 //and 100. Ignores a random amount of integer armor points, ranging from to 0 to
 //half of the armor the weapon hits.
 
-
-
-
-
-/*
-#include "CommonSword.h"
-
-double CommonSword::hit(double armor){
-    double damage = hitPoints - armor;
-    if(damage < 0){
-        return 0;
-    }
-    return damage;
+double CrazyRandomSword::hit(double armor){
+	armor = armor - (rand() % ((int)floor(armor) / 2));
+    int randInt = rand() % (100 - 10 + 1 ) + 10 // random int from 10-100
+    double damage = randInt - tempArmor;
+	return damage;
 }
-
-*/
